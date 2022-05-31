7f8e76a9b644812fdc120f5594f87267b8b4f86a
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-31 17:52:13 +0200

    index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @projects = Project.new
+  end
+
+
+  private
+
+  def projects_params
+    params.require(:project).permit(:funfact, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end
