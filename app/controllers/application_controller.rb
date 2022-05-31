d93b686ef0080e817ed5c00f3f91b423e85ecaec
Marion Socha <marion.socha@protonmail.com>
2022-05-31 19:43:58 +0200

    pundit  policy on new project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  include Pundit::Authorization
+  include Pundit
 
   # Pundit: white-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+    authorize @project
   end
 
   def create
