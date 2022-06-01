79680650da70f34f461a578a9f4f6f7f41a494bf
Marion Socha <marion.socha@protonmail.com>
2022-06-01 14:50:50 +0200

    start seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
+puts 'Creating scenarios...'
 scenario1 = Scenario.create([{ title: "Anniversaire", img: "anniversaire.jpeg"}])
 scenario2 = Scenario.create([{ title: "EVJF", img: "evjf.jpeg" }])
 scenario3 = Scenario.create([{ title: "mariages", img: "mariages.jpeg" }])
+puts '3 scenarios created!'
 
+puts 'Creating users...'
 user = User.create([{ email: "elodie@example", password: "elodie@example" }])
+puts '1 user created!'
 
 projet = Project.create([{ title: "Anniversaire de Lucie",description: "yeay c'est l'anniv de Lucie preparons-lui une surprise", user:  user} ])
+
+
+# db/seeds.rb
+
+
+chez_gladines = Restaurant.new(name: "Chez Gladines")
+chez_gladines.save!
+puts 'Finished!'
