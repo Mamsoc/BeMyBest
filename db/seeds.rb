9a1d06976baf57af6f898e8778647dfb5a8e9821
Marion Socha <marion.socha@protonmail.com>
2022-06-01 16:47:22 +0200

    add invitations and memories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+p "destroy memories"
+Memory.destroy_all
+p "destroy invitations"
+Invitation.destroy_all
 p "destroy projects"
 Project.destroy_all
+p "destroy users"
+User.destroy_all
 p "destroy scenarios"
 Scenario.destroy_all
-p "destroy user"
-User.destroy_all
 
 puts 'Creating scenarios...'
 scenario1 = Scenario.create(title: "Anniversaire", img: "anniversaire.jpeg")
@@ -20,16 +24,38 @@ scenario3 = Scenario.create(title: "mariages", img: "mariages.jpeg")
 puts '3 scenarios created!'
 
 puts 'Creating users...'
-user1 = User.create(email: "elodie@example.com", password: "elodie@example.com")
-user2 = User.create(email: "anthoine@example.com", password: "anthoine@example.com")
+user1 = User.create(email: "anthoine@example.com", password: "anthoine@example.com")
+user2 = User.create(email: "elodie@example.com", password: "elodie@example.com")
 user3 = User.create(email: "manu@example.com", password: "manu@example.com")
-user4 = User.create(email: "")
-puts '1 user created!'
+user4 = User.create(email: "lidwine@example.com", password: "lidwine@example.com")
+user5 = User.create(email: "hadrien@example.com", password: "hadrien@example.com")
+users = User.all
+
+puts '5 users created!'
 
 puts 'Creating projects...'
-projet = Project.create(title: "Anniversaire de Lucie",description: "yeay c'est l'anniv de Lucie preparons-lui une surprise", user:  user2)
+project = Project.create(title: "Anniversaire de Lucie", description: "yeay c'est l'anniv de Lucie preparons-lui une surprise", user: user1)
 puts '1 project created!'
 
 puts 'Creating invitations...'
+i = 0
+users.each do |user|
+  unless user == user1
+    p i
+    Invitation.create(email: user.email, code: '123456', user: user, project: project)
+
+    i += 1
+  end
+end
+puts "#{i} invitations created"
 
 puts 'Creating memories...'
+i = 0
+n = 0
+users.each do |user|
+  3.times do
+    Memory.create(legend: "légende #{(n + 1) * (i + 1)}", user: user, project: project)
+    n += 1
+  end
+end
+puts "#{n} memories created"
