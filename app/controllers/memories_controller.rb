fb1bf4fa57467a348be8b0c1df0e8f7ed6747fb2
hadrien albrecht <hadrien.albrecht@gmail.com>
2022-06-01 14:59:44 +0200

    controller et views memory

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,2 +1,57 @@
 class MemoriesController < ApplicationController
+  before_action :set_project
+  before_action :set_memory, except: [:create, :new]
+  def index
+    # authorize @memory
+    @memory = Memory.all
+
+  end
+
+  def edit
+    # authorize @memory
+  end
+
+  def update
+    # authorize @memory
+    @memory.update(memories_params)
+    redirect_to edit_project_memory_path(@memory)
+  end
+
+  def new
+    @memory = Memory.new
+    # authorize @memory
+  end
+
+  def create
+    @memory = Memory.new(memories_params)
+    @memory.user = current_user
+    # authorize @memory
+    @memory.legend = @memory.legend.capitalize
+    if @memory.save
+      redirect_to new_project_memory_path(@memory)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # authorize @memory
+    @memory.destroy
+    redirect_to project_memory_path(@memory)
+  end
+
+  private
+
+  def memories_params
+    params.require(:memory).permit(:legend, :description, photos: [])
+
+  end
+
+  def set_memory
+    @memory = Memory.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
 end
