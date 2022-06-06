3318e1ea3f754be1b6e35c2e90b4c1ce218270cc
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-06-06 14:27:56 +0200

    validation project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
   belongs_to :scenario, optional: true
   has_one_attached :photo
   has_many :memories
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :photo, presence: true
 end
